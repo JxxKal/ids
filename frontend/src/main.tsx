71fe9af629cda0c85c223c42b5d92a36ff0f9067
JxxKal <mail@jankaluza.de>
2026-05-04 18:11:40 +0200

    chore(main): i18n/extras.ts laden für hot-pluggable Translation-Bundles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HelpModeProvider } from './hooks/useHelpMode';
 import './i18n';
+import './i18n/extras';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
